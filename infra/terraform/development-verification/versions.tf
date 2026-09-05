f21fa31bb2ddd7252d3d405b0bc47857db51c338
Zakaria Ait Ali <zakaria-aitali@outlook.com>
2026-09-05 18:26:02 +0100

    Configure remote state for development verification

--- infra/terraform/development-verification/versions.tf
+++ infra/terraform/development-verification/versions.tf
@@ -1,6 +1,13 @@
 terraform {
   required_version = "= 1.15.9"
 
+  backend "s3" {
+    bucket  = "zero-to-prod-333534066371-eu-west-3-dev-verification-tfstate"
+    key     = "development-verification/terraform.tfstate"
+    region  = "eu-west-3"
+    encrypt = true
+  }
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
